9edb78129cc8cc5ead760e485a5a9312070bf81f
Jack Strong <jack_strong@163.com>
2019-08-01 09:56:03 +0800

    v0.0.1

--- PATHS.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime 
-version = datetime.now().strftime('%Y%m%d%H%M%S')
-print('version:{}'.format(version))
-feature_importance_path = '../importances/'
-
