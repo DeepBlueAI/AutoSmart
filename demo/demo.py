b94036d8f08cd39f96dab82b8beda0a895809406
DeepSmartAI <1229991666@qq.com>
2019-08-01 20:51:44 +0800

    update

--- demo/demo.py
+++ demo/demo.py
@@ -3,5 +3,6 @@ import auto_smart
 info = auto_smart.read_info("data")
 train_data,train_label = auto_smart.read_train("data",info)
 test_data = auto_smart.read_test("data",info)
+
 auto_smart.train_and_predict(train_data,train_label,info,test_data)
 
